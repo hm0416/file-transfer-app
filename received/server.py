32188ff303ace87594df5d7d703df4f3fade75d9
hm0416 <mousou.3@buckeyemail.osu.edu>
2021-03-19 21:57:03 -0400

    added cmds

--- received/server.py
+++ received/server.py
@@ -1,28 +1,30 @@
 import os
 import socket
 from datetime import datetime
+from os import listdir
+from os.path import isfile, join
 
 ### AUTHOR : HIFA MOUSOU ###
 
-#list all received files server has received
-def list_files():
-    pass
-
-#delete a received file
-def delete_files():
-    pass
-
-#get a list of cmds the sever can execute
-def get_cmds():
-    pass
-
-#quit program
-def quit_cmd():
-    pass
-
-#rename a received file on the sever
-def rename_files():
-    pass
+# #list all received files server has received
+# def list_files():
+#     pass
+#
+# #delete a received file
+# def delete_files():
+#     pass
+#
+# #get a list of cmds the sever can execute
+# def get_cmds():
+#     pass
+#
+# #quit program
+# def quit_cmd():
+#     pass
+#
+# #rename a received file on the sever
+# def rename_files():
+#     pass
 
 
 
@@ -51,11 +53,59 @@ while True:
     socket_connection.send("Filename received".encode("utf-8"))
     received_file_data = socket_connection.recv(1024).decode("utf-8")
     received_file.write(received_file_data)
-    socket_connection.send("File received.".encode("utf-8"))
+    socket_connection.send("File received.\n".encode("utf-8"))
+
+    print("File transferred to received folder!")
+
+    print("\nAvailable Commands You Can Enter: \n LIST: List all received files server has received \n DELETE: Delete a received file \n GET: Get a list of cmds the sever can execute \n QUIT: Quit the program \n RENAME: Rename a received file")
+
+    while True:
+        cmd = input("\nEnter Command: ")
+        if cmd == "GET":
+            print("Available Commands: \n LIST: List all received files server has received \n DELETE: Delete a received file \n GET: Get a list of cmds the sever can execute \n QUIT: Quit the program \n RENAME: Rename a received file")
+        if cmd == "QUIT":
+            received_file.close()
+            socket_connection.close()
+            print(f"Sever disconnected. You can find any transferred files in the received folder.")
+            quit()
+        if cmd == "LIST":
+            i = 1
+            list_of_received_files = [f for f in listdir(r"C:\Users\Hifam\PycharmProjects\file-transfer-app\received") if isfile(join(r"C:\Users\Hifam\PycharmProjects\file-transfer-app\received", f))]
+            for f in list_of_received_files:
+                if f == '.DS_Store' or f == 'server.py':
+                    pass
+                else:
+                    print(str(i) + '. ' + f + '\n')
+                    i += 1
+        if cmd == "DELETE":
+            user_input = input("What file do you want to delete?: ")
+            files_in_dir = os.listdir(r"C:\Users\Hifam\PycharmProjects\file-transfer-app\received")
+            while True:
+                if user_input in files_in_dir:
+                    # os.remove(r"C:\Users\Hifam\PycharmProjects\file-transfer-app\received\{user_input}")
+                    received_file.close()
+                    os.remove(user_input)
+                    print("File deleted successfully.")
+                    break
+                else:
+                    user_input = input("File not found. Enter another file to delete: ")
+        if cmd == "RENAME":
+            file_to_rename = input("Which file do you want to rename?: ")
+            new_name = input("What would you like to rename it to?: ")
+            files_in_dir_for_rn = os.listdir(r"C:\Users\Hifam\PycharmProjects\file-transfer-app\received")
+            directory = r"C:\Users\Hifam\PycharmProjects\file-transfer-app\received"
+            while True:
+                if file_to_rename in files_in_dir_for_rn:
+                    received_file.close()
+                    os.rename(directory + "\\" + file_to_rename, directory + "\\" + new_name)
+                    print("File renamed successfully.")
+                else:
+                    file_to_rename = input("File not found. Enter another file to rename: ")
 
     received_file.close()
-    socket_connection.close()
-    print(f"Sever disconnected. You can find the transferred file in the received folder.")
+
+socket_connection.close()
+# print(f"Sever disconnected. You can find the transferred file in the received folder.")
 
 
 
